930c86ae66fc62b471a162ac47486453bdec2696
Paul John toral <pjtoralme@gmail.com>
2025-11-29 09:57:23 +0800

    chore: fixed pubspec yaml

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   setupLocator();
   await LocalStorageService.init();
   runApp(const MyApp());
